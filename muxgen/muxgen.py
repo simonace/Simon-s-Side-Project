9279a9fbfc0fbb623c0da376e27b2067696c4cda
simonace <zjjxcai@gmail.com>
2018-07-26 23:16:51 +0800

    Initiated muxgen program
    This program is tended to be a generator of a net of multiplexers
    responding to a mapping table of pins and functions on them.

---
+++ muxgen/muxgen.py
@@ -0,0 +1,12 @@
+'''
+This program is tended to be a generator of a net of
+multiplexers responding to a mapping table of pins and
+functions on them.
+'''
+
+
+class Mux(object):
+
+class Pin(object):
+
+class Function(object):
